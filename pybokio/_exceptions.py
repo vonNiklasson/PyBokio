f30d5e38b5b65bdae3d68690dfdbbca7d573a0bc
Johan Niklasson <johan@niklasson.me>
2021-05-09 22:04:02 +0200

    Removed unused file

--- pybokio/_exceptions.py
+++
@@ -1,4 +0,0 @@
-from pybokio.exceptions import UnexpectedResponseError
-
-ExpectedJsonException = UnexpectedResponseError("Response expected to be JSON.")
-InvalidJsonSchemaException = UnexpectedResponseError("Invalid JSON schema from response.")
