a5df698bb6938e8dd3c2d3eaddb601da4acabb5a
Johan Niklasson <johan@niklasson.me>
2021-05-10 14:46:35 +0200

    Added tests for verification module

---
+++ tests/mocks/requests.py
@@ -0,0 +1,17 @@
+from typing import Dict
+
+from requests import Response
+
+
+class MockResponse(Response):
+
+    def __init__(self, content: str = None, headers: Dict = None, status_code: int = 200):
+        super(MockResponse, self).__init__()
+        if content:
+            self._content = content.encode()
+
+        if headers:
+            self.headers.update(headers)
+
+        if status_code:
+            self.status_code = status_code
---
+++ tests/unit/_utils/test_verification.py
@@ -0,0 +1,27 @@
+from pybokio._utils.verification import is_valid_uuid4, is_response_json
+from tests.mocks.requests import MockResponse
+
+
+class TestIsValidUuid4:
+    def test_valid_uuid(self):
+        assert is_valid_uuid4('00000000-0000-4000-8000-000000000000') is True
+
+    def test_invalid_uuid(self):
+        assert is_valid_uuid4('00000000-0000-0000-0000-000000000000') is False
+
+    def test_invalid_uuid_format(self):
+        assert is_valid_uuid4('no') is False
+
+
+class TestIsResponseJson:
+    def test_valid_json_response(self):
+        response = MockResponse(content="{}", headers={"Content-Type": "application/json"})
+        assert is_response_json(response) is True
+
+    def test_invalid_content_type_response(self):
+        response = MockResponse(content="{}", headers={"Content-Type": "html"})
+        assert is_response_json(response) is False
+
+    def test_invalid_content_response(self):
+        response = MockResponse(content="Just basic text", headers={"Content-Type": "application/json"})
+        assert is_response_json(response) is False
