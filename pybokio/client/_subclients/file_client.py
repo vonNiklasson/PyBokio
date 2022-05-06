aaa80964af88d81948555de2291113dece7b07bf
Johan Niklasson <johan@niklasson.me>
2022-05-06 17:02:53 +0200

    Added models

--- pybokio/client/_subclients/file_client.py
+++ pybokio/client/_subclients/file_client.py
@@ -4,6 +4,7 @@ from requests import Response
 
 from pybokio._routers.file_routers import FileDeleteReceiptsRouter, FileListReceiptsRouter, FileUploadReceiptPdfRouter
 from pybokio.client._subclients._base_sub_client import BaseSubClient
+from pybokio.models.file import Receipt
 from pybokio.options import FileUploadReceiptCategories
 
 
@@ -13,8 +14,8 @@ class FileClient(BaseSubClient):
 
     def upload_receipt_pdf(
         self, file_path, category: FileUploadReceiptCategories = FileUploadReceiptCategories.UNKNOWN
-    ) -> str:
-        data = {"selectedType": category.value, "startRecepiptPredictionsNow": True}
+    ) -> Receipt:
+        data = {"selectedType": category.value, "startReceiptPredictionsNow": True}
         files = {"file": open(file_path, "rb")}
         endpoint = FileUploadReceiptPdfRouter()
         response: Response = self.client.call_api(**endpoint.kwargs, data=data, files=files)
@@ -22,25 +23,27 @@ class FileClient(BaseSubClient):
         endpoint.validate_response(response)
         res = response.json()
 
-        return res["Id"]
+        return Receipt(**res)
 
-    def list_receipts(self, include_bookkept: bool = False) -> List[str]:
+    def list_receipts(self, include_bookkept: bool = False) -> List[Receipt]:
         endpoint = FileListReceiptsRouter(include_bookkept=include_bookkept)
         response: Response = self.client.call_api(**endpoint.kwargs)
 
         endpoint.validate_response(response)
         res = response.json()
 
-        return [receipt["Id"] for receipt in res["Data"]["Receipts"]]
+        return [Receipt(**receipt) for receipt in res["Data"]["Receipts"]]
 
-    def delete_receipts(self, receipts: Union[str, List[str]]) -> None:
-        if type(receipts) is str:
-            receipts = [receipts]
+    def delete_receipts(self, receipts: List[Union[str, Receipt]]) -> None:
+        receipt_ids = [receipt.Id if isinstance(receipt, Receipt) else receipt for receipt in receipts]
 
-        payload = {"ReceiptIds": receipts}
+        payload = {"ReceiptIds": receipt_ids}
 
         endpoint = FileDeleteReceiptsRouter()
         response: Response = self.client.call_api(**endpoint.kwargs, json=payload)
 
         endpoint.validate_response(response)
         res = response.json()
+
+    def delete_receipt(self, receipt: Union[str, Receipt]) -> None:
+        return self.delete_receipts([receipt])
---
+++ pybokio/models/file/receipt.py
@@ -0,0 +1,38 @@
+from types import SimpleNamespace
+from typing import Any, List, Optional
+
+
+class Receipt(SimpleNamespace):
+    class ExtraPage(SimpleNamespace):
+        Id: str
+        ReceiptId: str
+        PageNumber: int
+        IsPdf: bool
+
+    def __init__(self, **kwargs: Any):
+        super().__init__(**kwargs)
+
+        if "ExtraPages" in kwargs:
+            self.ExtraPages = [self.ExtraPage(**page) for page in kwargs["ExtraPages"]]
+
+    Description: Optional[str]
+    PredictedTotalSum: Optional[float]
+    PredictedPaymentDate: Optional[str]
+    PredictedDate: Optional[str]
+    Category: str
+    VerificationId: Optional[str]
+    PaymentDate: Optional[str]
+    TotalSum: Optional[float]
+    AutoVerificationRowId: Optional[str]
+    IsPdf: bool
+    Attested: bool
+    IsAutoVerificationRowPending: bool
+    IsNonSignedOrScheduledPayment: bool
+    PaymentStatus: Optional[Any]
+    ManualPaymentId: Optional[str]
+    Id: str
+    UploadedDate: Optional[str]
+    Date: Optional[str]
+    Version: int
+    UploadedByUser: str
+    ExtraPages: List[ExtraPage]
